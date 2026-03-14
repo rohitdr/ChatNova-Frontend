b98cca8ccd91d2eaf4645a045b6a81098a3fce93
rohit <rohitdr098@gmail.com>
2026-03-14 10:12:44 +0530

    updated vit congif

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: { 
-'/api': {
-    target: import.meta.env.VITE_SOCKET,
-    changeOrigin: true,
-    secure: false,
-  }
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_SOCKET,
+          changeOrigin: true,
+          secure: false
+        }
+      }
     }
-  }
-})
+  })
+}
